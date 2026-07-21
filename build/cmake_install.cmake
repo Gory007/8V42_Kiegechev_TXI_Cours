ab14d9c3fac0c48d34a27f3f97ea3b7268a842b9
Goryoo7 <kiegechev09@gmail.com>
2026-07-21 16:40:38 +0700

    Исправление релизов

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,12 +37,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/works/Course_4sem/Chess960/build/_deps/googletest-build/cmake_install.cmake")
-
-endif()
-
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
